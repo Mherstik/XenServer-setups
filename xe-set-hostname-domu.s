9890d148c8e57f5ca81446f2e253ed11ee3674dd
Marcus Herstik <marcus@herstik.com>
2015-12-26 12:31:58 +1100

    Create xe-set-hostname-domu.s

---
+++ xe-set-hostname-domu.s
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Based on https://github.com/krobertson/xenserver-automater/blob/master/usr/sbin/xe-set-hostname
+# Adapted by Frederick Ding
+# based on https://raw.githubusercontent.com/frederickding/xenserver-kickstart/develop/opt/domu-hostname.sh
+
+# Detect if xenstore-read is available
+XENSTOREREAD=`which xenstore-read`
+if [ -e $XENSTOREREAD ]; then
+	# Filter the domU's name to a lowercase alphanumeric hyphenated hostname
+	NAME=`xenstore-read name | sed -e 's/[^[:alnum:]|-]/-/g' | tr '[:upper:]' '[:lower:]'`
+
+	# Don't do anything if this name is blank
+	[ "$NAME" = "" ] && exit 0
+
+	# Set the hostname
+	echo "$NAME" > /etc/hostname
+	/bin/hostname -F /etc/hostname
+fi
+exit 0
